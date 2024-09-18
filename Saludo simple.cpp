0c5dcd9dfe48a96aae5481c027d7283ea908f2bd
CristianTambaco <168015193+CristianTambaco@users.noreply.github.com>
2024-09-18 10:18:06 -0500

    Add files via upload

---
+++ Saludo simple.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	//Declaración de variables
+	 string nombre = "";
+	
+	cout<<"Ingrese su nombre:"<< endl;
+	cin>> nombre;
+	cout<<"Hola "<<nombre<< endl;
+	
+	return 0;
+}
